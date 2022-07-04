0a7239ee8d23371d0cb1b6b95036258c723a4a35
Igor Marçal <107085324+IgorMarcal@users.noreply.github.com>
2022-07-04 00:58:13 -0300

    Add files via upload

--- Questao3/Distribuidora3/Program.cs
+++ Questao3/Distribuidora3/Program.cs
@@ -24,8 +24,7 @@ namespace Distribuidora3
                 
                 foreach (var elemento in items)
                 {
-                    //Console.WriteLine($"Dia:{elemento.dia} Valor:{elemento.valor}");
-
+                    
                     if(elemento.valor > 0 && primeiraVez)
                     {
                         min = elemento.valor;
@@ -69,44 +68,6 @@ namespace Distribuidora3
         {
 
             LoadJson();
-
-
-
-
-
-            /*
-            try
-            {
-                // Create an instance of StreamReader to read from a file.
-                // The using statement also closes the StreamReader.
-
-                using (StreamReader sr = new StreamReader("dados.json"))
-                {
-                    string line;
-                    // Read and display lines from the file until the end of
-                    // the file is reached.
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        Console.WriteLine(line);
-                    }
-
-                    dynamic array = JsonConvert.DeserializeObject(line);
-                    foreach (var item in array)
-                    {
-                        Console.WriteLine("{0} {1}", item.dia, item.valor);
-                    }
-
-                }
-            }
-            catch(Exception e)
-            {
-                // Let the user know what went wrong.
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
-
-            */
-
 
         }
     }
